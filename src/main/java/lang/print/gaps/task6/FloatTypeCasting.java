ef7712643a02f7e8e36f433866b9af54f0e04965
InDev <InDevRus@hotmail.com>
2023-03-09 04:08:19 +0500

    Solved

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        var rounded = Math.round(numberToBeRounded);
+        System.out.println(rounded);
     }
 }
--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        var first = 1000L;
+        var second = (int) first;
+        System.out.println(first);
+        System.out.println(second);
     }
 }
